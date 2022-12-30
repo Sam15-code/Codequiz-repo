4ddc9a434dd216312bf7ad852665784dd5bbc08e
Sam15-code <ibtissam.dxb@gmail.com>
2022-12-30 16:40:51 -0600

    Adding files and base layout

---
+++ assets/questions.js
@@ -0,0 +1,42 @@
+var questionList = [
+    {
+        question: "HTML is ",
+        choices1:"Hyper take markip list",
+        choices2:"Hypertext Markup Language",
+        choices3:"abcd",
+        choices4:"eiwu",
+        answer:"choices2"
+    },
+    {
+        question: "HTML is ",
+        choices1:"Hyper take markip list",
+        choices2:"Hypertext Markup Language",
+        choices3:"abcd",
+        choices4:"eiwu",
+        answer:"choices2"
+    },
+    {
+        question: "HTML is ",
+        choices1:"Hyper take markip list",
+        choices2:"Hypertext Markup Language",
+        choices3:"abcd",
+        choices4:"eiwu",
+        answer:"choices2"
+    },
+    {
+        question: "HTML is ",
+        choices1:"Hyper take markip list",
+        choices2:"Hypertext Markup Language",
+        choices3:"abcd",
+        choices4:"eiwu",
+        answer:"choices2"
+    },
+    {
+        question: "HTML is --- END ",
+        choices1:"Hyper take markip list",
+        choices2:"Hypertext Markup Language",
+        choices3:"abcd",
+        choices4:"eiwu",
+        answer:"choices2"
+    }
+]
---
+++ assets/script.js
@@ -0,0 +1,5 @@
+var mainEl = document.getElementById("main")
+var resultsEl = document.getElementById("results")
+
+mainEl.style.display = "none"
+resultsEl.style.display = "none"
