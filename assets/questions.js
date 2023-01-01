6091b9d3f75b69e869c6284e8bbac66fa81e6570
Sam15-code <ibtissam.dxb@gmail.com>
2022-12-31 22:25:52 -0600

    added questions and choices

--- assets/questions.js
+++ assets/questions.js
@@ -1,42 +1,42 @@
 var questionList = [
     {
-        question: "HTML is ",
-        choices1:"Hyper take markip list",
-        choices2:"Hypertext Markup Language",
-        choices3:"abcd",
-        choices4:"eiwu",
+        question: "In which Italian city can you find the Colosseum? ",
+        choices1:"Venice",
+        choices2:"Rome",
+        choices3:"Naple",
+        choices4:"Milan",
         answer:"choices2"
     },
     {
-        question: "HTML is ",
-        choices1:"Hyper take markip list",
-        choices2:"Hypertext Markup Language",
-        choices3:"abcd",
-        choices4:"eiwu",
-        answer:"choices2"
+        question: "In the TV show New Girl, which actress plays Jessica Day?",
+        choices1:"Zooey Deschanel",
+        choices2:"Kaley Cuoco",
+        choices3:"Jennifer Aniston",
+        choices4:"Alyson Hannigan",
+        answer:"choices1"
     },
     {
-        question: "HTML is ",
-        choices1:"Hyper take markip list",
-        choices2:"Hypertext Markup Language",
-        choices3:"abcd",
-        choices4:"eiwu",
-        answer:"choices2"
+        question: "What is the largest canyon in the world?",
+        choices1:"Verdon Gorge, France",
+        choices2:"King’s Canyon, Australia",
+        choices3:"Grand Canyon, USA",
+        choices4:"Fjaðrárgljúfur Canyon, Iceland",
+        answer:"choices3"
     },
     {
-        question: "HTML is ",
-        choices1:"Hyper take markip list",
-        choices2:"Hypertext Markup Language",
-        choices3:"abcd",
-        choices4:"eiwu",
-        answer:"choices2"
+        question: "How long is the border between the United States and Canada?",
+        choices1:"3,525 miles",
+        choices2:"4,525 miles",
+        choices3:"5,525 miles",
+        choices4:"6,525 miles",
+        answer:"choices3"
     },
     {
-        question: "HTML is --- END ",
-        choices1:"Hyper take markip list",
-        choices2:"Hypertext Markup Language",
-        choices3:"abcd",
-        choices4:"eiwu",
-        answer:"choices2"
+        question: "What is the largest active volcano in the world? --- END ",
+        choices1:"Mount Etna",
+        choices2:"Mount Vesuvius",
+        choices3:"Mouna Loa ",
+        choices4:"Mount Batur",
+        answer:"choices3"
     }
 ]
